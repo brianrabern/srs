2d8dec6163ee0893640ce4acb7451fb85fbf72d8
Brian <brian.rabern@gmail.com>
2023-11-20 09:06:48 -0800

    refactored

--- database.py
+++ database.py
@@ -2,97 +2,122 @@ import mysql.connector
 from hash import generate_uuid
 import time
 
-connection = mysql.connector.connect(
-    user="admin",
-    password="password",
-    host="db",  # if using docker-compose
-    # host="127.0.0.1",  # if using local mariadb
-    port=3306,
-    database="srsdb",
-)
+
+def connect():
+    return mysql.connector.connect(
+        user="admin",
+        password="password",
+        host="127.0.0.1",
+        port=3306,
+        database="srsdb",
+    )
 
 
 def get_all():
-    cursor = connection.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM users")
-    users = cursor.fetchall()
-    return {"users": users}
+    query = "SELECT * FROM users"
+    cursor = None
+    try:
+        connection = connect()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(query)
+        users = cursor.fetchall()
+        return {"users": users}
+    finally:
+        cursor.close() if cursor else None
+        connection.close() if connection else None
 
 
 def get_one(uuid):
-    cursor = connection.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM users WHERE uuid = %s", (uuid,))
-    user = cursor.fetchone()
-    return {"user": user}
+    query = "SELECT * FROM users WHERE uuid = %s"
+    cursor = None
+    try:
+        connection = connect()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(query, (uuid,))
+        user = cursor.fetchone()
+        return {"user": user}
+    finally:
+        cursor.close() if cursor else None
+        connection.close() if connection else None
 
 
 def post_one(contact):
-    values = {"uuid": generate_uuid(contact)}
-    if "@" in contact:
-        values["email"] = contact
-        values["sms"] = None
-    else:
-        values["sms"] = contact
-        values["email"] = None
-    values["username"] = None
-    values["name"] = None
-    current_timestamp = int(time.time())
-    values["created"] = current_timestamp
-    values["lastseen"] = current_timestamp
-
-    cursor = connection.cursor(dictionary=True)
-    cursor.execute(
-        "INSERT INTO users (uuid, username, name, email, sms, created, lastseen) VALUES (%s, %s, %s, %s, %s, %s, %s)",  # noqa
-        (
-            values["uuid"],
-            values["username"],
-            values["name"],
-            values["email"],
-            values["sms"],
-            values["created"],
-            values["lastseen"],
-        ),
+    insert_query = (
+        "INSERT INTO users (uuid, username, name, email, sms, created, lastseen) "  # noqa
+        "VALUES (%(uuid)s, %(username)s, %(name)s, %(email)s, %(sms)s, %(created)s, %(lastseen)s)"  # noqa
     )
-    connection.commit()
-    cursor.execute("SELECT * FROM users WHERE uuid = %s", (values["uuid"],))
-    user = cursor.fetchone()
-    return {"user": user}
+
+    values = {
+        "uuid": generate_uuid(contact),
+        "username": None,
+        "name": None,
+        "email": contact if "@" in contact else None,
+        "sms": contact if "@" not in contact else None,
+        "created": int(time.time()),
+        "lastseen": int(time.time()),
+    }
+    cursor = None
+    try:
+        connection = connect()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(insert_query, values)
+        connection.commit()
+
+        select_query = "SELECT * FROM users WHERE uuid = %s"
+        cursor.execute(select_query, [values["uuid"]])
+        user = cursor.fetchone()
+
+        return {"user": user}
+
+    finally:
+        cursor.close() if cursor else None
+        connection.close() if connection else None
 
 
 def update_one(uuid, update_data: dict):
-    update_data_filtered = {
-        key: value
-        for key, value in update_data.items()
-        if key in ["username", "name", "email", "sms"]
-    }
-
-    cursor = connection.cursor(dictionary=True)
-
     update_query = "UPDATE users SET "
-    update_values = []
+    update_values = {}
 
-    for key, value in update_data_filtered.items():
-        update_query += f"{key} = %s, "
-        update_values.append(value)
+    for key, value in update_data.items():
+        if key in ["username", "name", "email", "sms"]:
+            update_query += f"{key} = %({key})s, "
+            update_values[key] = value
 
-    update_query += "lastseen = %s"
-    update_values.append(int(time.time()))
+    update_query += "lastseen = %(lastseen)s"
+    update_values["lastseen"] = int(time.time())
 
-    update_query += " WHERE uuid = %s"
-    update_values.append(uuid)
+    update_query += " WHERE uuid = %(uuid)s"
+    update_values["uuid"] = uuid
 
-    cursor.execute(update_query, tuple(update_values))
-    connection.commit()
+    cursor = None
 
-    updated_user = get_one(uuid)
-    return {"user": updated_user}
+    try:
+        connection = connect()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(update_query, update_values)
+        connection.commit()
+
+        updated_user = get_one(uuid)
+        return {"user": updated_user}
+    finally:
+        cursor.close() if cursor else None
+        connection.close() if connection else None
 
 
 def delete_one(uuid):
-    cursor = connection.cursor(dictionary=True)
-    cursor.execute("DELETE FROM users WHERE uuid = %s", (uuid,))
-    connection.commit()
-    if cursor.rowcount:
-        return {"status": "success", "deleted_user": uuid}
-    else:
-        return {"status": "failed"}
+    cursor = None
+    try:
+        connection = connect()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("DELETE FROM users WHERE uuid = %s", (uuid,))
+        connection.commit()
+
+        return (
+            {"status": "success", "deleted_user": uuid}
+            if cursor.rowcount
+            else {"status": "failed"}
+        )
+
+    finally:
+        cursor.close() if cursor else None
+        connection.close() if connection else None
